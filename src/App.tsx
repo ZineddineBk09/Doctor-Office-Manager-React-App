1f67952495de3762970d932112c134726408a3b6
ZineddineBk09 <eino1209@gmail.com>
2024-06-23 22:00:44 +0100

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.tsx
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import Login from "./components/login";
+import Dashboard from "./components/dashboard";
+import Appointments from "./components/appointments";
+import MainLayout from "./layouts/main";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+
+          <Route
+            path="/dashboard"
+            element={
+              <MainLayout>
+                <Dashboard />
+              </MainLayout>
+            }
+          />
+          <Route
+            path="/appointments"
+            element={
+              <MainLayout>
+                <Appointments />
+              </MainLayout>
+            }
+          />
+          <Route path="/patients" element={<div />} />
+          <Route path="/messages" element={<div />} />
+          <Route path="/profile" element={<div />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
