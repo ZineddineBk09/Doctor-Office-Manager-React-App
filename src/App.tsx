f6185231478a560ffcba37e485463ca036142129
ZineddineBk09 <eino1209@gmail.com>
2024-06-25 14:10:37 +0100

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,33 +7,74 @@ import Login from "./components/login";
 import Dashboard from "./components/dashboard";
 import Appointments from "./components/appointments";
 import MainLayout from "./layouts/main";
+import Patients from "./components/patients";
+import Chats from "./components/chats";
+import ProtectedRoute from "./components/shared/protected-route";
+import Signup from "./components/register";
+import UnProtectedRoute from "./components/shared/unprotected-route";
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
-
+          <Route path="/" element={<UnProtectedRoute element={<Login />} />} />
+          <Route
+            path="/register"
+            element={<UnProtectedRoute element={<Signup />} />}
+          />
           <Route
             path="/dashboard"
             element={
-              <MainLayout>
-                <Dashboard />
-              </MainLayout>
+              <ProtectedRoute
+                element={
+                  <MainLayout>
+                    <Dashboard />
+                  </MainLayout>
+                }
+              />
             }
           />
           <Route
             path="/appointments"
             element={
-              <MainLayout>
-                <Appointments />
-              </MainLayout>
+              <ProtectedRoute
+                element={
+                  <MainLayout>
+                    <Appointments />
+                  </MainLayout>
+                }
+              />
             }
           />
-          <Route path="/patients" element={<div />} />
-          <Route path="/messages" element={<div />} />
-          <Route path="/profile" element={<div />} />
+          <Route
+            path="/patients"
+            element={
+              <ProtectedRoute
+                element={
+                  <MainLayout>
+                    <Patients />
+                  </MainLayout>
+                }
+              />
+            }
+          />
+          <Route
+            path="/chats"
+            element={
+              <ProtectedRoute
+                element={
+                  <MainLayout>
+                    <Chats />
+                  </MainLayout>
+                }
+              />
+            }
+          />
+          <Route
+            path="/profile"
+            element={<ProtectedRoute element={<div />} />}
+          />
         </Routes>
       </Router>
     </Provider>
