87c5e6da6fcc19dabcb93288437a81c76977b27e
ZineddineBk09 <eino1209@gmail.com>
2024-06-28 11:52:07 +0100

    fix

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     "Content-Type": "application/json",
-    "x-api-key": process.env.REACT_APP_API_KEY,
+    // "x-api-key": process.env.REACT_APP_API_KEY,
     Accept: "application/json",
     "Access-Control-Allow-Origin": "*",
   },
